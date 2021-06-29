d4c1fda61c4790df453b6ccd3f8be2a0737800ca
RM <RRMoelker@users.noreply.github.com>
2021-06-29 19:19:34 +0200

    Recording and playback with delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const AudioContext = window.AudioContext;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [supported, setSupported] = useState<boolean | string | undefined>(undefined);
+    const [devices, setDevices] = useState<InputDeviceInfo[]>([]);
+    const [micId, setMicId] = useState<string | undefined>();
+    const [stream, setStream] = useState<MediaStream | undefined>()
+
+    const checkSupported = () => {
+        if (navigator.mediaDevices && AudioContext) {
+            setSupported(true);
+        } else {
+            setSupported('The audio features required are not found on your browser.');
+        }
+    }
+    useEffect(checkSupported, []);
+
+    const getMicrophones = async () => {
+        navigator.mediaDevices.enumerateDevices().then((devices) => {
+            devices = devices.filter((d) => d.kind === 'audioinput');
+            setDevices(devices as InputDeviceInfo[]);
+        });
+    }
+
+    const start = async () => {
+        if (!micId) {
+            throw new Error('missing capture device id');
+        }
+        const stream: MediaStream = await navigator.mediaDevices.getUserMedia(
+            {
+                // audio: true // constraints - only audio needed for this app
+                audio: {
+                    deviceId: micId,
+                }
+            })
+        setStream(stream);
+
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaStreamSource(stream);
+
+        const delayNode = audioContext.createDelay();
+        delayNode.delayTime.value = 1; // seconds
+
+        source.connect(delayNode).connect(audioContext.destination);
+    }
+
+    const stop = () => turnOffStream();
+
+    const turnOffStream = () => {
+        if (stream) {
+            stream.getTracks().forEach(track => track.stop());
+        }
+        setStream(undefined);
+    }
+
+    const switchMic = (deviceId: string) => {
+        turnOffStream();
+        setMicId(deviceId);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {supported === undefined
+                && <>
+                  <p>Checking browser for Audio APIs</p>
+                </>}
+                {supported === true
+                && <>
+                  <div>
+                    <p>
+                      Record from an audio input device and play it back with a delay.
+                    </p>
+                    <p className={'small'}>
+                      Remember to not use the same input and audio device unless you want to go into a feedback loop.
+                    </p>
+                    <p>
+                        {devices.length === 0 && <button onClick={getMicrophones}>Detect available devices</button>}
+                    </p>
+                  </div>
+
+                    {devices.length > 0 && <>
+                      <div>{stream
+                          ? <div>
+                              Recording {stream && <button onClick={stop}>Stop</button>}
+                          </div>
+                          : <div>
+                              Not recording {micId && <button onClick={start}>Record</button>}
+                          </div>
+                      }
+                      </div>
+                      <h2>Devices</h2>
+                      <ol className={'devices'}>
+                          {devices.map(d => (
+                              <li
+                                  key={d.deviceId}
+                                  className={micId === d.deviceId ? 'active' : ''}
+                                  onClick={() => switchMic(d.deviceId)}
+                              >{d.label}</li>
+                          ))}
+                      </ol>
+                    </>}
+                </>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
